88981b332c3b2bf0448807a95d467b7755ff7e00
prostopluxury <56890176+prostopluxury@users.noreply.github.com>
2019-12-08 20:04:26 +0300

    add item touch helper

---
+++ Tinder/app/src/main/java/com/example/tinder/ItemTouchHelperAdapter.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Alexander Lomat
+ */
+
+
+package com.example.tinder;
+
+/**
+ * The interface Item touch helper adapter.
+ */
+public interface ItemTouchHelperAdapter {
+    /**
+     * On item swiped left.
+     */
+    void onItemSwipedLeft();
+
+    /**
+     * On item swiped right.
+     */
+    void onItemSwipedRight();
+}
