2a39bd86bb324d1cc72ab55de36c9eb8aa47c526
prostopluxury <56890176+prostopluxury@users.noreply.github.com>
2019-12-08 19:52:41 +0300

    add fragment switching

--- Tinder/app/src/main/java/com/example/tinder/MainActivity.java
+++ Tinder/app/src/main/java/com/example/tinder/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.tinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type Main activity.
+ */
+public class MainActivity extends AppCompatActivity implements FragmentHelper {
+
+    private MainFragment mainFragment;
+    private ResultFragment resultFragment;
+    private FragmentManager fragmentManager;
+
+    private List<TinderImage> likedItems;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        likedItems = new ArrayList<>();
+
+        mainFragment = new MainFragment();
+        resultFragment = new ResultFragment();
+
+        fragmentManager = getSupportFragmentManager();
+
+        onFragmentSwitched(FragmentType.MAIN);
+    }
+
+    @Override
+    public void onFragmentSwitched(FragmentType type) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (type) {
+            case MAIN:
+                fragmentTransaction.replace(R.id.contentPage, mainFragment);
+                break;
+            case RESULT:
+                fragmentTransaction.replace(R.id.contentPage, resultFragment);
+                break;
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public List<TinderImage> getLikedItems() {
+        return likedItems;
+    }
+
+    @Override
+    public void setLikedItems(List<TinderImage> likedItems) {
+        this.likedItems = likedItems;
     }
 }
